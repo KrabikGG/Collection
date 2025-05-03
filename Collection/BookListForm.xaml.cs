f656e3268c95694d2b8e9a5fd6d888a42aebd71a
Андрій Городецький <elit23520095@ms.sumdu.edu.ua>
2025-05-03 14:16:53 +0300

    Добавьте файлы проекта.

---
+++ Collection/BookListForm.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Collection
+{
+    /// <summary>
+    /// Логика взаимодействия для BookListForm.xaml
+    /// </summary>
+    public partial class BookListForm : Window
+    {
+        public BookListForm()
+        {
+            InitializeComponent();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            AddDataForm AddWnd = new AddDataForm();
+            AddWnd.Show();
+            //this.Visibility = Visibility.Collapsed;
+        }
+
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            EditDataForm EditWnd = new EditDataForm();
+            EditWnd.Show();
+            //this.Visibility = Visibility.Collapsed;
+        }
+    }
+}
